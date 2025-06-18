9eabbbffcc8efe88a8bec9b17e02b10b45e3eb9e
XpycTee <i@xpyctee.ru>
2025-06-18 10:46:21 +0500

    Add rate limiting for proxy requests and update configuration for Steam API

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import datetime
 import ssl
+import time
 import certifi
 from flask import Flask, request, Response
 import urllib.request
@@ -7,6 +9,8 @@ import yaml
 
 app = Flask(__name__)
 
+# где-то в модуле, например, в app.config или глобальном dict:
+LAST_REQUESTS = {}
 
 def get_urls():
     with open('config.yaml', "r", encoding="utf-8") as config_file:
@@ -20,6 +24,21 @@ def proxy(prefix, endpoint):
     if not url_info or 'url' not in url_info:
         return Response("Unknown prefix", status=404)
     base_url = url_info['url']
+    rate_limit = url_info.get('rate_limit')
+
+    if rate_limit:
+        # Время между запросами в секундах
+        query_delay_sec = 60.0 / rate_limit
+
+        now = datetime.now()
+        last = LAST_REQUESTS.get(prefix)
+
+        if last is not None:
+            elapsed = (now - last).total_seconds()
+            wait = query_delay_sec - elapsed
+            if wait > 0:
+                time.sleep(wait)
+
     url = f"{base_url}/{endpoint}"
     headers = {key: value for key, value in request.headers if key != 'Host'}
     context = ssl.create_default_context(cafile=certifi.where())
@@ -43,7 +62,7 @@ def proxy(prefix, endpoint):
                                    if k.lower() not in ['content-encoding', 'content-length', 'transfer-encoding', 'connection']]
         else:
             return Response("Метод не поддерживается", status=405)
-
+        LAST_REQUESTS[prefix] = datetime.now()
         return Response(content, status, response_headers)
     except Exception as e:
         return Response(f"Ошибка при обращении к Bybit API: {e}", status=500)
